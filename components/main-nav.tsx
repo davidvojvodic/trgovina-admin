96f7b9cbb2429f7e3a38499266582bf78086828c
David Vojvodić <davidvojvodic1@gmail.com>
2023-08-27 21:56:13 +0200

    store switcher

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { cn } from "@/lib/utils";
+import Link from "next/link";
 import { useParams, usePathname } from "next/navigation";
 
 export function MainNav({
@@ -11,13 +12,30 @@ export function MainNav({
   const params = useParams();
   const routes = [
     {
-      href: `/${params.storeId}`,
+      href: `/${params.storeId}/settings`,
+      label: "Settings",
+      active: pathname === `/${params.storeId}/settings`,
     },
   ];
 
   return (
     <nav
       className={(cn("flex items-center space-x-4 lg:space-x-6"), className)}
-    ></nav>
+    >
+      {routes.map((route) => (
+        <Link
+          key={route.href}
+          href={route.label}
+          className={cn(
+            "text-sm font-medium transition-colors hover:text-primary",
+            route.active
+              ? "text-black dark:text-white"
+              : "text-muted-foreground"
+          )}
+        >
+          {route.label}
+        </Link>
+      ))}
+    </nav>
   );
 }
