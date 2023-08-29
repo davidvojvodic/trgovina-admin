993cce0b45d1d1207a91d8be6e9245d605d788d2
David Vojvodić <davidvojvodic1@gmail.com>
2023-08-29 11:51:31 +0200

    image upload

--- components/billboard-client.tsx
+++ components/billboard-client.tsx
@@ -4,8 +4,11 @@ import { Plus } from "lucide-react";
 import Heading from "./heading";
 import { Button } from "./ui/button";
 import { Separator } from "./ui/separator";
+import { useParams, useRouter } from "next/navigation";
 
 const BillboardClient = () => {
+  const router = useRouter();
+  const params = useParams();
   return (
     <>
       <div className="flex items-center justify-between">
@@ -13,7 +16,9 @@ const BillboardClient = () => {
           title="Billboards (0)"
           description="Manage billboards for your store"
         />
-        <Button>
+        <Button
+          onClick={() => router.push(`/${params.storeId}/billboards/new`)}
+        >
           <Plus className="mr-2 w-4 h-4" />
           Add New
         </Button>
