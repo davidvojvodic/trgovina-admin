f318e267ed17a6ecf845920256fa8e511ae65fe4
David Vojvodić <97889188+davidvojvodic@users.noreply.github.com>
2024-05-01 13:00:30 +0200

    improve

--- app/(dashboard)/[storeId]/(routes)/attributes/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/attributes/page.tsx
@@ -1,26 +1,37 @@
 import prismadb from "@/lib/prismadb";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { AttributeColumn } from "./components/attribute-columns";
 import { format } from "date-fns";
 import AttributeClient from "./components/attribute-client";
 
-const AttributesPage = async ({ params }: { params: { storeId: string } }) => {
-  const attributes = await prismadb.attribute.findMany({
-    where: {
-      storeId: params.storeId,
-    },
-  });
+const AttributesPage = ({ params }: { params: { storeId: string } }) => {
+  const [attributes, setAttributes] = useState<AttributeColumn[]>([]);
 
-  const formattedAttributes: AttributeColumn[] = attributes.map((item) => ({
-    id: item.id,
-    key: item.key,
-    value: item.value,
-    createdAt: format(item.createdAt, "MMMM do, yyyy"),
-  }));
+  useEffect(() => {
+    const fetchAttributes = async () => {
+      const data = await prismadb.attribute.findMany({
+        where: {
+          storeId: params.storeId,
+        },
+      });
+
+      const formattedAttributes: AttributeColumn[] = data.map((item) => ({
+        id: item.id,
+        key: item.key,
+        value: item.value,
+        createdAt: format(item.createdAt, "MMMM do, yyyy"),
+      }));
+
+      setAttributes(formattedAttributes);
+    };
+
+    fetchAttributes();
+  }, [params.storeId]);
+
   return (
     <div className="flex-col">
       <div className="flex-1 space-y-4 p-8 pt-6">
-        <AttributeClient data={formattedAttributes} />
+        <AttributeClient data={attributes} />
       </div>
     </div>
   );
