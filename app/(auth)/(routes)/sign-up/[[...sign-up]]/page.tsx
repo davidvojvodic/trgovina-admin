cfb9a5c4148b69dfa231f91a872d78ff6fae248a
David Vojvodić <97889188+davidvojvodic@users.noreply.github.com>
2024-05-01 13:00:29 +0200

    improve

--- app/(auth)/(routes)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/(routes)/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,19 @@
 import { SignUp } from "@clerk/nextjs";
 
-export default function Page() {
-  return <SignUp />;
+export default function Page({ onSignUp }) {
+  return (
+    <SignUp
+      formFields={[
+        "email_address",
+        "first_name",
+        "last_name",
+        "password",
+        "phone_number",
+      ]}
+      onSignUp={(frontendApiResponse) => {
+        // Do something with the sign-up response
+        onSignUp(frontendApiResponse);
+      }}
+    />
+  );
 }
