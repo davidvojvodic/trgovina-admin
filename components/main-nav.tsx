7332618d771af39054a2d163d9d25bfdadfb3bdd
David Vojvodić <97889188+davidvojvodic@users.noreply.github.com>
2024-05-01 13:00:38 +0200

    improve

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -3,89 +3,37 @@ import { cn } from "@/lib/utils";
 import Link from "next/link";
 import { useParams, usePathname } from "next/navigation";
 
-export function MainNav({
-  className,
-  ...props
-}: React.HTMLAttributes<HTMLElement>) {
-  // Get the current URL pathname using the usePathname hook.
+export function MainNav({ className, ...props }) {
   const pathname = usePathname();
-
-  // Get route parameters from the URL using the useParams hook.
   const params = useParams();
 
-  // Define an array of route configurations for navigation links.
   const routes = [
-    {
-      href: `/${params.storeId}`,
-      label: "Dashboard",
-      // Check if the current pathname matches this route to mark it as active.
-      active: pathname === `/${params.storeId}`,
-    },
-    {
-      href: `/${params.storeId}/billboards`,
-      label: "Billboards",
-      active: pathname === `/${params.storeId}/billboards`,
-    },
-    {
-      href: `/${params.storeId}/categories`,
-      label: "Categories",
-      active: pathname === `/${params.storeId}/categories`,
-    },
-    {
-      href: `/${params.storeId}/sizes`,
-      label: "Sizes",
-      active: pathname === `/${params.storeId}/sizes`,
-    },
-    {
-      href: `/${params.storeId}/colors`,
-      label: "Colors",
-      active: pathname === `/${params.storeId}/colors`,
-    },
-    {
-      href: `/${params.storeId}/attributes`,
-      label: "Attributes",
-      active: pathname === `/${params.storeId}/attributes`,
-    },
-    {
-      href: `/${params.storeId}/products`,
-      label: "Products",
-      active: pathname === `/${params.storeId}/products`,
-    },
-    {
-      href: `/${params.storeId}/orders`,
-      label: "Orders",
-      active: pathname === `/${params.storeId}/orders`,
-    },
-    // {
-    //   href: `/${params.storeId}/enquiries`,
-    //   label: "Enquiries",
-    //   active: pathname === `/${params.storeId}/enquiries`,
-    // },
-    {
-      href: `/${params.storeId}/settings`,
-      label: "Settings",
-      active: pathname === `/${params.storeId}/settings`,
-    },
+    { path: `/${params.storeId}`, name: "Dashboard" },
+    { path: `/${params.storeId}/billboards`, name: "Billboards" },
+    { path: `/${params.storeId}/categories`, name: "Categories" },
+    { path: `/${params.storeId}/sizes`, name: "Sizes" },
+    { path: `/${params.storeId}/colors`, name: "Colors" },
+    { path: `/${params.storeId}/attributes`, name: "Attributes" },
+    { path: `/${params.storeId}/products`, name: "Products" },
+    { path: `/${params.storeId}/orders`, name: "Orders" },
+    // { path: `/${params.storeId}/enquiries`, name: "Enquiries" },
+    { path: `/${params.storeId}/settings`, name: "Settings" },
   ];
 
   return (
-    <nav
-      className={cn("flex items-center space-x-4 lg:space-x-6", className)}
-      {...props}
-    >
-      {/* Render navigation links based on the route configuration. */}
+    <nav className={cn("flex items-center space-x-4 lg:space-x-6", className)} {...props}>
       {routes.map((route) => (
         <Link
-          key={route.href}
-          href={route.href}
+          key={route.path}
+          href={route.path}
           className={cn(
             "text-sm font-medium transition-colors hover:text-primary",
-            route.active
+            pathname === route.path
               ? "text-black dark:text-white"
               : "text-muted-foreground"
           )}
         >
-          {route.label}
+          {route.name}
         </Link>
       ))}
     </nav>
