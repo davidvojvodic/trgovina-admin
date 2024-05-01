e27eca8cc01bae3c8c9390f67385497d911fe396
David Vojvodić <97889188+davidvojvodic@users.noreply.github.com>
2024-05-01 13:00:25 +0200

    improve

--- actions/get-sales-count.ts
+++ actions/get-sales-count.ts
@@ -1,15 +1,17 @@
 import prismadb from "@/lib/prismadb";
 
-export const getSalesCount = async (storeId: string) => {
+export const getSalesCount = async (storeId: string): Promise<number> => {
+  try {
     const salesCount = await prismadb.order.count({
-        where: {
-            storeId,
-            isPaid: true,
-        },
-        
+      where: {
+        storeId,
+        isPaid: true,
+      },
     });
 
-    
-
     return salesCount;
-}
+  } catch (error) {
+    console.error("Error while fetching sales count:", error);
+    throw error;
+  }
+};
