574c7bbb32ddf6a8e805a5139944cb5000319ab0
David Vojvodić <97889188+davidvojvodic@users.noreply.github.com>
2024-05-01 13:00:34 +0200

    improve

--- app/api/[storeId]/attributes/[attributeId]/route.ts
+++ app/api/[storeId]/attributes/[attributeId]/route.ts
@@ -7,19 +7,24 @@ export async function GET(
   req: Request,
   { params }: { params: { storeId: string; attributeId: string } }
 ) {
-  try {
-    // Step 1: Check if the 'storeId' parameter is missing in the request, and if so, return a 400 Bad Request response
-    if (!params.storeId) {
-      return new NextResponse("Store ID is required", { status: 400 });
-    } else if (!params.attributeId) {
-      return new NextResponse("Attribute ID is required", { status: 400 });
-    }
+  if (!params.storeId || !params.attributeId) {
+    return new NextResponse(
+      params.storeId ? "Attribute ID is required" : "Store ID is required",
+      { status: 400 }
+    );
+  }
 
+  try {
     const attribute = await prismadb.attribute.findUnique({
       where: {
         id: params.attributeId,
       },
     });
+
+    if (!attribute) {
+      return new NextResponse("Attribute not found", { status: 404 });
+    }
+
     return NextResponse.json(attribute);
   } catch (error) {
     console.log("[ATTRIBUTE_GET]", error);
@@ -31,96 +36,12 @@ export async function PATCH(
   req: Request,
   { params }: { params: { storeId: string; attributeId: string } }
 ) {
-  try {
-    const { userId } = auth();
+  const { userId } = auth();
 
-    const body = await req.json();
-
-    const { key, value } = body;
-
-    if (!userId) {
-      return new NextResponse("Unauthorized", { status: 401 });
-    }
-
-    if (!key) {
-      return new NextResponse("Key is required", { status: 400 });
-    }
-
-    if (!value) {
-      return new NextResponse("Value is required", { status: 400 });
-    }
-
-    if (!params.attributeId) {
-      return new NextResponse("Attribute ID is required", { status: 400 });
-    }
-
-    const storeByUser = await prismadb.store.findFirst({
-      where: {
-        id: params.storeId,
-        userId: userId,
-      },
-    });
-
-    if (!storeByUser) {
-      return new NextResponse(
-        `User ${userId} does not have access to this store`,
-        { status: 403 }
-      );
-    }
-
-    const attribute = await prismadb.attribute.update({
-      where: {
-        id: params.attributeId,
-      },
-      data: {
-        key,
-        value,
-      },
-    });
-    return NextResponse.json(attribute);
-  } catch (error) {
-    console.log("[ATTRIBUTE_PATCH]", error);
-    return new NextResponse("Internal error", { status: 500 });
+  if (!userId) {
+    return new NextResponse("Unauthorized", { status: 401 });
   }
-}
 
-export async function DELETE(
-  req: Request,
-  { params }: { params: { storeId: string; attributeId: string } }
-) {
-  try {
-    const { userId } = auth();
-
-    if (!userId) {
-      return new NextResponse("Unauthorized", { status: 401 });
-    }
-
-    if (!params.attributeId) {
-      return new NextResponse("Attribute ID is required", { status: 400 });
-    }
-
-    const storeByUser = await prismadb.store.findFirst({
-      where: {
-        id: params.storeId,
-        userId: userId,
-      },
-    });
-
-    if (!storeByUser) {
-      return new NextResponse(
-        `User ${userId} does not have access to this store`,
-        { status: 403 }
-      );
-    }
-
-    const attribute = await prismadb.attribute.delete({
-      where: {
-        id: params.attributeId,
-      },
-    });
-    return NextResponse.json(attribute);
-  } catch (error) {
-    console.log("[ATTRIBUTE_DELETE]", error);
-    return new NextResponse("Internal error", { status: 500 });
-  }
-}
+  if (!params.attributeId) {
+    return new NextResponse("Attribute ID is required", { status: 400 });
+ 
