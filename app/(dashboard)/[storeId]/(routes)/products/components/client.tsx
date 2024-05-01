eb76b06d0ad729bdf1fcdccaa3ed8e1c9b598ab4
David Vojvodić <97889188+davidvojvodic@users.noreply.github.com>
2024-05-01 13:00:34 +0200

    improve

--- app/(dashboard)/[storeId]/(routes)/products/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/products/components/client.tsx
@@ -1,20 +1,9 @@
 "use client";
-/**
- * Product Client Component
- *
- * This component displays a list of products with options to add a new product.
- * It also provides an API list for products.
- *
- * @param {Object} props - Component props
- * @param {ProductColumn[]} props.data - The product data to display
- * @returns {JSX.Element} - The rendered ProductClient component
- */
 import { Plus } from "lucide-react";
 import Heading from "../../../../../../components/heading";
 import { Button } from "../../../../../../components/ui/button";
 import { Separator } from "../../../../../../components/ui/separator";
 import { useParams, useRouter } from "next/navigation";
-
 import { ProductColumn, columns } from "./columns";
 import { DataTable } from "../../../../../../components/data-table";
 import { ApiList } from "../../../../../../components/api-list";
@@ -27,27 +16,27 @@ const ProductClient = ({ data }: ProductClientProps) => {
   const router = useRouter();
   const params = useParams();
 
+  const addProductHandler = () => {
+    router.push(`/${params.storeId}/products/new`);
+  };
+
   return (
     <>
-      {/* Display heading with the total number of products */}
       <div className="flex items-center justify-between">
         <Heading
           title={`Products (${data.length})`}
           description="Manage products for your store"
         />
-        {/* Button to navigate to the page for adding a new product */}
-        <Button onClick={() => router.push(`/${params.storeId}/products/new`)}>
+        <Button onClick={addProductHandler}>
           <Plus className="mr-2 w-4 h-4" />
           Create new
         </Button>
       </div>
       <Separator />
-      {/* Display a data table for products */}
       <DataTable columns={columns} data={data} />
-      {/* Display a heading for the API section */}
+      <Separator />
       <Heading title="API" description="API calls for products" />
       <Separator />
-      {/* Display an API list for products */}
       <ApiList entityName="products" entityIdName="productId" />
     </>
   );
