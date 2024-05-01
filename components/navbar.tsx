17b2c717d1b568ed64db95b6ca7160e917228c10
David Vojvodić <97889188+davidvojvodic@users.noreply.github.com>
2024-05-01 13:00:36 +0200

    improve

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -6,38 +6,41 @@ import prismadb from "@/lib/prismadb";
 import { ThemeToggle } from "./theme-toggle";
 
 const Navbar = async () => {
-  // Get the current user's ID using the auth() function from Clerk.
-  const { userId } = auth();
+  const user = auth();
 
-  // If the user is not authenticated, redirect them to the sign-in page.
+  if (!user.getSessionId()) {
+    redirect("/sign-in");
+  }
+
+  const userId = user.getSessionId()?.userId;
+
   if (!userId) {
     redirect("/sign-in");
   }
 
-  // Fetch a list of stores associated with the user from the database.
-  const stores = await prismadb.store.findMany({
-    where: {
-      userId: userId,
-    },
-  });
+  try {
+    const stores = await prismadb.store.findMany({
+      where: {
+        userId: userId,
+      },
+    });
 
-  return (
-    <div className="border-b">
-      <div className="flex h-16 items-center px-4">
-        {/* Render the store switcher component with the fetched stores. */}
-        <StoreSwitcher items={stores} />
-
-        {/* Render the main navigation component. */}
-        <MainNav className="mx-6" />
-
-        <div className="ml-auto flex items-center space-x-4">
-          {/* Render the UserButton component for user actions. */}
-          <ThemeToggle />
-          <UserButton afterSignOutUrl="/" />
+    return (
+      <div className="border-b">
+        <div className="flex h-16 items-center px-4">
+          <StoreSwitcher items={stores} />
+          <MainNav className="mx-6" />
+          <div className="ml-auto flex items-center space-x-4">
+            <ThemeToggle />
+            <UserButton afterSignOutUrl="/" />
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } catch (error) {
+    console.error("Error fetching stores:", error);
+    redirect("/sign-in");
+  }
 };
 
 export default Navbar;
