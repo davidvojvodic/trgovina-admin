df534e2683ba0fff84d4dae83decf35510e47426
David Vojvodić <97889188+davidvojvodic@users.noreply.github.com>
2024-05-01 13:00:52 +0200

    improve

--- hooks/use-store-modal.tsx
+++ hooks/use-store-modal.tsx
@@ -1,20 +1,20 @@
 import { create } from "zustand";
 
 // Define the interface for the store
-interface useStoreModalStore {
-  isOpen: boolean; // A boolean to track whether the modal is open or not
-  onOpen: () => void; // A function to open the modal
-  onClose: () => void; // A function to close the modal
+interface UseStoreModalStore {
+  isModalOpen: boolean; // A boolean to track whether the modal is open or not
+  openModal: () => void; // A function to open the modal
+  closeModal: () => void; // A function to close the modal
 }
 
 // Create and export the Zustand store
-export const useStoreModal = create<useStoreModalStore>((set) => ({
-  // Step 1: Initialize the store with isOpen set to false, meaning the modal is initially closed.
-  isOpen: false,
+export const useStoreModal = create<UseStoreModalStore>((set) => ({
+  // Step 1: Initialize the store with isModalOpen set to false, meaning the modal is initially closed.
+  isModalOpen: false,
 
-  // Step 2: Define the onOpen function to set isOpen to true, opening the modal.
-  onOpen: () => set({ isOpen: true }),
+  // Step 2: Define the openModal function to set isModalOpen to true, opening the modal.
+  openModal: () => set({ isModalOpen: true }),
 
-  // Step 3: Define the onClose function to set isOpen to false, closing the modal.
-  onClose: () => set({ isOpen: false }),
+  // Step 3: Define the closeModal function to set isModalOpen to false, closing the modal.
+  closeModal: () => set({ isModalOpen: false }),
 }));
