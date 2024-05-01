dd740ac9fc15b8b794de9f3f250ead88723be38e
David Vojvodić <97889188+davidvojvodic@users.noreply.github.com>
2024-05-01 13:00:33 +0200

    improve

--- app/(dashboard)/[storeId]/(routes)/categories/components/category-client.tsx
+++ app/(dashboard)/[storeId]/(routes)/categories/components/category-client.tsx
@@ -1,26 +1,17 @@
 "use client"; // Notable import statement
-// Components and Actions:
-// 1. Import necessary modules and components
-// 2. Define the CategoryClient component
-//    - Takes data as a prop, representing category column data
-//    - Renders a page for managing categories
-//    - Displays a heading with the count of categories and a description
-//    - Provides a button to add a new category
-//    - Renders a DataTable to display category data
-//    - Displays a heading for API calls related to categories
-//    - Renders an APIList component for category API calls
 
 // Import necessary modules and components
-
 import { Plus } from "lucide-react";
-import Heading from "../../../../../../components/heading";
-import { Button } from "../../../../../../components/ui/button";
-import { Separator } from "../../../../../../components/ui/separator";
+import Heading from "../../../../components/heading";
+import { Button } from "../../../../components/ui/button";
+import { Separator } from "../../../../components/ui/separator";
 import { useParams, useRouter } from "next/navigation";
-
-import { DataTable } from "../../../../../../components/data-table";
-import { ApiList } from "../../../../../../components/api-list";
-import { CategoryColumn, categoriesColumns } from "./category-colums";
+import { DataTable } from "../../../../components/data-table";
+import { ApiList } from "../../../../components/api-list";
+import {
+  CategoryColumn,
+  categoriesColumns,
+} from "./category-columns";
 
 // Define the props interface for the CategoryClient component
 interface CategoryClientProps {
@@ -31,12 +22,13 @@ interface CategoryClientProps {
 const CategoryClient = ({ data }: CategoryClientProps) => {
   const router = useRouter();
   const params = useParams();
+
   return (
     <>
       {/* Render a section with heading, description, and a button to add a new category */}
       <div className="flex items-center justify-between">
         <Heading
-          title={`Categories (${data.length})`} // Display the count of categories
+          title={`Categories (Count: ${data.length})`} // Display the count of categories
           description="Manage categories for your store" // Description
         />
         <Button
