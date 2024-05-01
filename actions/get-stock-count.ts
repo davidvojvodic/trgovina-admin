235eca19b2524735837f6a13c710e4c281b83cef
David Vojvodić <97889188+davidvojvodic@users.noreply.github.com>
2024-05-01 13:00:26 +0200

    improve

--- actions/get-stock-count.ts
+++ actions/get-stock-count.ts
@@ -1,15 +1,17 @@
 import prismadb from "@/lib/prismadb";
 
-export const getStockCount = async (storeId: string) => {
+export const getStockCount = async (storeId: string): Promise<number> => {
+  try {
     const stockCount = await prismadb.product.count({
-        where: {
-            storeId,
-            isArchived: false
-        },
-        
+      where: {
+        storeId,
+        isArchived: false,
+      },
     });
 
-    
-
     return stockCount;
-}
+  } catch (error) {
+    console.error("Error while fetching stock count:", error);
+    throw error;
+  }
+};
