b6ee0cbd00c944512407a87d4cd51a38d2c6185f
David Vojvodić <97889188+davidvojvodic@users.noreply.github.com>
2024-05-01 13:00:49 +0200

    improve

--- providers/modal-provider.tsx
+++ providers/modal-provider.tsx
@@ -1,24 +1,19 @@
 "use client";
-// Import necessary components and hooks from external libraries and components
-import { StoreModal } from "@/components/modals/store-modal"; // Import StoreModal component from a relative path
-import { useEffect, useState } from "react"; // Import useEffect and useState hooks from React library
+import { StoreModal } from "@/components/modals/store-modal";
+import { useEffect, useState } from "react";
 
-// Define a component called `ModalProvider`
 export const ModalProvider = () => {
-  // Step 1: Define a state variable `isMounted` and initialize it to `false`
-  const [isMounted, setIsMounted] = useState(false);
+  const [isInitialMount, setIsInitialMount] = useState(true);
 
-  // Step 2: Use the `useEffect` hook to set `isMounted` to `true` when the component mounts
   useEffect(() => {
-    setIsMounted(true);
+    setIsInitialMount(false);
   }, []);
 
-  // Step 3: Check if the component is not yet mounted, and if so, return `null` (don't render anything)
-  if (!isMounted) {
+  // Only render the modal when the component has finished initializing
+  if (isInitialMount) {
     return null;
   }
 
-  // Step 4: Render the `StoreModal` component inside a fragment
   return (
     <>
       <StoreModal />
